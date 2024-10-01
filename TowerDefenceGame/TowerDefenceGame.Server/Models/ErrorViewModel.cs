4399eff953af4aadc0ab8b1f3696916818d4472d
ugnspa <125737106+ugnspa@users.noreply.github.com>
2024-10-01 18:29:14 +0300

    init

---
+++ TowerDefenceGame/TowerDefenceGame.Server/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TowerDefenceGame.Server.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
