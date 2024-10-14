0eda81772c3c2d9ffcbde2fe04ee2fc80e7ccdd9
Edv1nas <Edv1nas>
2024-10-14 22:44:18 +0300

    Fixed builder pattern

---
+++ Server/Models/Towers/TowerBuilder.cs
@@ -0,0 +1,20 @@
+﻿using TowerDefense.Enums;
+
+namespace TowerDefense.Models.Towers
+{
+    public abstract class TowerBuilder
+    {
+        protected Tower _tower;
+        protected TowerCategories _category;
+
+        public TowerBuilder(TowerCategories category)
+        {
+            _category = category;
+        }
+
+        public abstract void BuildBase(int x, int y);
+        public abstract void AddWeapon();
+        public abstract void AddArmor();
+        public abstract Tower GetResult();
+    }
+}
