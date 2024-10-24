6c79c73efd1f41c064ca3f8b916e664b0c91fa85
Edvinas <126004821+edvral@users.noreply.github.com>
2024-10-24 21:02:50 +0300

    Resolved requested changes

--- Server/Program.cs
+++ Server/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", builder =>
-        builder.WithOrigins("http://localhost:5700")
+        builder.WithOrigins("http://localhost:3000")
                .AllowAnyMethod()
                .AllowAnyHeader()
                .AllowCredentials());
